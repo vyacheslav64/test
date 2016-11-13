c16a16996358060c3c8e86b471db60c65d4710f5
123 <123@wih>
2016-11-13 13:39:12 +0300

    create

---
+++ UchetT/src/Ustroystvo.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Ustroystvo {
+	public String type;
+	public int sku;
+	public String name;
+	public Date dateOfNew;
+	//необходим метод создания строки об устройстве!
+}
